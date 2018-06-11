2b18c79a21f249f64cebacd079b777eafdcaca99
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2018-06-11 13:25:35 +0200

    Add User entity

--- AuthenticationServer.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AuthenticationServer.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ AuthenticationServer.Domain/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AuthenticationServer.Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime LastLogin { get; set; }
+    }
+}
