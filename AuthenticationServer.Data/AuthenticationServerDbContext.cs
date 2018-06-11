a1fc3c89f50fa7ed81c729a5cfd7ff02b67d2488
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2018-06-11 14:59:37 +0200

    Add migration

--- AuthenticationServer.Data/AuthenticationServerDbContext.cs
+++ AuthenticationServer.Data/AuthenticationServerDbContext.cs
@@ -1,11 +1,17 @@
 ﻿using System;
-using Microsoft.EntityFrameworkCore;
 using AuthenticationServer.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace AuthenticationServer.Data
 {
-    public class AuthenticationServerDbContext:DbContext
+    public class AuthenticationServerDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connection = @"Server=DESKTOP-NJESQAR;Database=AuthenticationServer;Trusted_Connection=True;MultipleActiveResultSets=true";
+            optionsBuilder.UseSqlServer(connection);
+        }
     }
 }
