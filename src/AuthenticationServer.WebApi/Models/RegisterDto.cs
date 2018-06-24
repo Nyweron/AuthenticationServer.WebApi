43e607a53fbdaae4c71ac9af9801b1ee1846a7d6
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2018-06-24 18:33:09 +0200

    Add required props

--- src/AuthenticationServer.WebApi/Models/RegisterDto.cs
+++ src/AuthenticationServer.WebApi/Models/RegisterDto.cs
@@ -4,11 +4,24 @@ namespace AuthenticationServer.WebApi.Models
 {
     public class RegisterDto
     {
-        [Required]
+        [Required(ErrorMessage = "The Email must be submitted", AllowEmptyStrings = false)]
         public string Email { get; set; }
-        [Required]
+        [Required(ErrorMessage = "The Password must be submitted", AllowEmptyStrings = false)]
         [StringLength(100)]
         public string Password { get; set; }
         public string Role { get; set; }
+
+        [Required(ErrorMessage = "The FirstName must be submitted", AllowEmptyStrings = false)]
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        [MaxLength(30)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "The LastName must be submitted", AllowEmptyStrings = false)]
+        [MaxLength(40)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "The Login must be submitted", AllowEmptyStrings = false)]
+        [MaxLength(20)]
+        public string Login { get; set; }
     }
 }
